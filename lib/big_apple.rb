5363f0869b70a975e41b44befed6f52e9132640e
Renée De Voursney <renee.devoursney@gmail.com>
2012-04-26 22:32:35 -0700

    initial test and code

---
+++ lib/big_apple.rb
@@ -0,0 +1,6 @@
+class String
+  
+def as_new_yorker
+  self.split(' ').join(' fuck ')
+end
+end
--- test/test_big_apple.rb
+++ test/test_big_apple.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
 class TestBigApple < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "insert fuck as every other word" do
+    assert_equal("good morning".as_new_yorker, "good fuck morning")
   end
 end
